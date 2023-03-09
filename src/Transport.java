2044f1c8d69470266dd577bafb3d3d6c7f1f3b15
Александр <perchatki79@gmail.com>
2023-03-09 16:22:15 +0300

    third commit

--- src/Transport.java
+++ src/Transport.java
@@ -1,6 +1,6 @@
-public class Transport {
-    private String modelName;
-    private int wheelsCount;
+public abstract class Transport {
+    private final String modelName;
+    private final int wheelsCount;
 
     public Transport(String modelName, int wheelsCount) {
         this.modelName = modelName;
